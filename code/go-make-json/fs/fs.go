d5c40298da724b2c65a7433a684c753de4930514
Tom Opgenorth <tom@opgenorth.net>
2021-08-02 10:49:53 -0600

    Delete the JSON file if it exists.

--- code/go-make-json/fs/fs.go
+++ code/go-make-json/fs/fs.go
@@ -9,15 +9,18 @@ import (
 	"io/ioutil"
 	"opgenorth.net/labradar/labradar"
 	"opgenorth.net/labradar/util"
+	"os"
 )
 
 const DirectoryForJson = "/Users/tom/work/topgenorth.github.io/data/labradar/"
 
 func LoadLabradarSeriesFromCsv(seriesNumber int, ls *labradar.Series) error {
+	filename := util.GetPathToLabradarSeries(seriesNumber)
 	a := afero.Afero{
 		Fs: afero.NewOsFs(),
 	}
-	file, err := a.Open(util.GetPathToLabradarSeries(seriesNumber))
+
+	file, err := a.Open(filename)
 	defer func(f afero.File) {
 		err := f.Close()
 		if err != nil {
@@ -44,18 +47,31 @@ func LoadLabradarSeriesFromCsv(seriesNumber int, ls *labradar.Series) error {
 }
 
 func SaveLabradarSeriesToJson(ls *labradar.Series) error {
+
+	filename := ls.JsonFileName(DirectoryForJson)
+	a := afero.Afero{
+		Fs: afero.NewOsFs(),
+	}
+
+	exists, _ := a.Exists(filename)
+	if exists {
+		e1 := os.Remove(filename)
+		if e1 != nil {
+			jww.ERROR.Println(e1)
+		}
+	}
+
 	jsonToSave, err := json.MarshalIndent(ls, "", "  ")
 	if err != nil {
 		return err
 	}
 
-	filename :=ls.JsonFileName(DirectoryForJson)
 	err2 := ioutil.WriteFile(filename, jsonToSave, 0644)
 	if err2 != nil {
 		return err2
 	}
 
-	jww.INFO.Printf("Saved the JSON to %s.",filename )
+	jww.INFO.Printf("Saved the JSON to %s.", filename)
 
 	fmt.Println(string(jsonToSave))
 	return nil
