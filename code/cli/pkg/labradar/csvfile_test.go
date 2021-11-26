b32c1fe05a3b85e45b58bb74d009082772e93a38
Tom Opgenorth <tom@opgenorth.net>
2021-11-26 14:43:25 -0700

    Removing some variables.

--- code/cli/pkg/labradar/csvfile_test.go
+++ code/cli/pkg/labradar/csvfile_test.go
@@ -5,32 +5,22 @@ import . "github.com/onsi/gomega"
 import "testing"
 
 var _ = Describe("isLabradarCsvFile", func() {
-	var (
-		valid    string
-		invalid1 string
-		invalid2 string
-	)
-
-	BeforeEach(func() {
-		valid = "/tmp/data/LBR/SR0001/SR0001 Report.csv"
-		invalid1 = "/tmp/data/LBR/SR0001/SR0001 Report.txt"
-		invalid2 = "/tmp/data/LBR/SR0001/0001 Report.csv"
-	})
 
 	Describe("Check if Labradar CSV ", func() {
+
 		Context("Has correct prefix and extension", func() {
 			It("should pass.", func() {
-				Expect(isLabradarCsvFile(valid)).To(BeTrue())
+				Expect(isLabradarCsvFile("/tmp/data/LBR/SR0001/SR0001 Report.csv")).To(BeTrue())
 			})
 		})
 		Context("Has invalid extension", func() {
 			It("should not pass", func() {
-				Expect(isLabradarCsvFile(invalid1)).To(BeFalse())
+				Expect(isLabradarCsvFile("/tmp/data/LBR/SR0001/SR0001 Report.txt")).To(BeFalse())
 			})
 		})
 		Context("Does not start with SR", func() {
 			It("should not pass", func() {
-				Expect(isLabradarCsvFile(invalid2)).To(BeFalse())
+				Expect(isLabradarCsvFile("/tmp/data/LBR/SR0001/0001 Report.csv")).To(BeFalse())
 			})
 		})
 	})
