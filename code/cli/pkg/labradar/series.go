ce7b5e919ddccc8b08177fd9b9b70d52b6d6ff9f
Tom Opgenorth <tom.opgenorth@intelligentimagingystems.com>
2022-01-17 09:22:02 -0700

    Moved seriesbuilder into series.go

--- code/cli/pkg/labradar/series.go
+++ code/cli/pkg/labradar/series.go
@@ -227,3 +227,49 @@ func LoadSeries(filename string, fs aferox.Aferox) (*Series, error) {
 type SeriesWriter interface {
 	Write(s Series) error
 }
+
+type SeriesBuilder struct {
+	*Series
+	RawData map[int]*LineOfData `json:"data"`
+}
+
+func NewSeriesBuilder() *SeriesBuilder {
+	return &SeriesBuilder{
+		NewSeries(),
+		make(map[int]*LineOfData),
+	}
+}
+
+func (sb *SeriesBuilder) ParseLine(ld *LineOfData) {
+	switch ld.LineNumber {
+	case 1:
+		sb.RawData[ld.LineNumber] = ld
+		sb.Labradar.DeviceId = ld.StringValue()
+	case 3:
+		sb.Series.Number = ld.IntValue()
+		sb.Labradar.SeriesName = "SR" + ld.StringValue()
+		sb.RawData[ld.LineNumber] = ld
+	case 6:
+		sb.RawData[ld.LineNumber] = ld
+		sb.Labradar.Units.Velocity = ld.StringValue()
+	case 7:
+		sb.RawData[ld.LineNumber] = ld
+		sb.Labradar.Units.Distance = ld.StringValue()
+	case 9:
+		sb.RawData[ld.LineNumber] = ld
+		sb.Labradar.Units.Weight = ld.StringValue()
+	case 18:
+		// For now, we only care about V0 (i.e. the muzzle velocity).
+		sb.RawData[ld.LineNumber] = ld
+		sb.Velocities.AddVelocity(ld.IntValue())
+
+		// We also pull the date and time from the first shot recorded
+		sb.Labradar.Date, sb.Labradar.Time = ld.DateAndTime()
+
+	default:
+		if ld.LineNumber > 18 {
+			sb.RawData[ld.LineNumber] = ld
+			sb.Velocities.AddVelocity(ld.IntValue())
+		}
+	}
+}
