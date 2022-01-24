19a1862360496957ec4a495bdbd30645d0398c4b
Tom Opgenorth <tom@opgenorth.net>
2022-01-23 19:37:46 -0700

    Write a series to a JSON file.

--- code/cli/pkg/labradar/writers.go
+++ code/cli/pkg/labradar/writers.go
@@ -1,10 +1,13 @@
 package labradar
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/afero"
 	"opgenorth.net/mylittlerangebook/pkg/config"
 	"opgenorth.net/mylittlerangebook/pkg/labradar/series"
+	"text/template"
 )
 
 // SeriesWriter is an interface to persisting a OldSeries to something a person might read (HTML, JSON, Markdown, etc).
@@ -42,6 +45,44 @@ func (w *JsonSeriesWriter) Write(s series.LabradarSeries) error {
 
 	return nil
 }
+
+type ReadMeSeriesWriter struct {
+	Output    string
+	OldFormat bool
+}
+
+func (w *ReadMeSeriesWriter) Write(s series.LabradarSeries) error {
+	var tmpl string
+	if w.OldFormat {
+		tmpl = TMPL_README_LINE_OLD_FORMAT
+	} else {
+		tmpl = TMPL_README_LINE
+	}
+
+	t, err := template.New("ToStringSerialWriter").Parse(tmpl)
+	if err != nil {
+		return series.SeriesError{
+			Number: s.Number,
+			Msg:    fmt.Sprintf("Error creating text.template: %v", err),
+		}
+	}
+
+	var line bytes.Buffer
+	err = t.Execute(&line, s)
+	if err != nil {
+		return series.SeriesError{
+			Number: s.Number,
+			Msg:    fmt.Sprintf("Error writing template: %v", err),
+		}
+	}
+
+	w.Output = line.String()
+
+	return nil
+}
+
+const TMPL_README_LINE = `| {{.Number}} | {{.LoadData}} | {{.Firearm.Name}} | {{.Notes}} | {{.Labradar.Date}} |`
+const TMPL_README_LINE_OLD_FORMAT = `| {{.Number}} | {{.LoadData}};{{.Notes}} | {{.Firearm.Name}} | {{.Labradar.Date}} |`
 
 const TMPL_DESCRIBE_SERIES = `
 # Description of Labradar series
