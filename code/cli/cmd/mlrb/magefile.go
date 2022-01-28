6ee999f37750eb4c52998c0316cb51ccc65b63e9
Tom Opgenorth <tom@opgenorth.net>
2022-01-28 16:50:39 -0700

    Flush out the magefile

--- code/cli/cmd/mlrb/magefile.go
+++ code/cli/cmd/mlrb/magefile.go
@@ -8,37 +8,42 @@ import (
 	"os"
 	"os/exec"
 
-	mg "github.com/magefile/mage/mg" // mg contains helpful utility functions, like Deps
+	mg "github.com/magefile/mage/mg"
 )
 
-// Default target to run when none is specified
-// If not set, running mage will list available targets
-// var Default = Build
+const (
+	compiledApp = "C:\\Users\\tom.opgenorth\\code\\MyLittleRangeBook\\bin\\mlrb.exe"
+)
 
 // A build step that requires additional params, or platform specific steps for example
 func Build() error {
 	mg.Deps(InstallDeps)
-	fmt.Println("Building...")
-	cmd := exec.Command("go", "build", "-o", "MyApp", ".")
+	fmt.Printf("Building %s...\n", compiledApp)
+	cmd := exec.Command("go", "build", "-o", compiledApp, "C:\\Users\\tom.opgenorth\\code\\MyLittleRangeBook\\code\\cli\\cmd\\mlrb\\main.go")
 	return cmd.Run()
 }
 
 // A custom install step if you need your bin someplace other than go/bin
 func Install() error {
 	mg.Deps(Build)
-	fmt.Println("Installing...")
-	return os.Rename("./MyApp", "/usr/bin/MyApp")
+	fmt.Printf("Installing %s...\n", compiledApp)
+	return os.Rename(compiledApp, "C:\\Users\\tom.opgenorth\\code\\MyLittleRangeBook\\data\\mlrb.exe")
 }
 
 // Manage your deps, or running package managers.
 func InstallDeps() error {
 	fmt.Println("Installing Deps...")
-	cmd := exec.Command("go", "get", "github.com/stretchr/piglatin")
-	return cmd.Run()
+	//cmd := exec.Command("go", "get", "github.com/stretchr/piglatin")
+	//return cmd.Run()
+	return nil
 }
 
 // Clean up after yourself
 func Clean() {
-	fmt.Println("Cleaning...")
-	os.RemoveAll("MyApp")
+	fmt.Println("Cleaning")
+	fmt.Printf("  > deleting %s\n", compiledApp)
+	err := os.RemoveAll(compiledApp)
+	if err != nil {
+		return
+	}
 }
