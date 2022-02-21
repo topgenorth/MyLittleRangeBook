0fc5e1722fe42842481f9dc61d797b2f350eee25
Tom Opgenorth <tom@opgenorth.net>
2022-02-20 19:33:17 -0700

    WIP

--- code/cli/pkg/persistence/persistence.go
+++ code/cli/pkg/persistence/persistence.go
@@ -2,16 +2,22 @@ package persistence
 
 import (
 	"fmt"
+	"github.com/mattn/go-sqlite3"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+)
+
+const (
+	DatabaseName = "mlrb.db3"
 )
 
 type Cartridge struct {
 	gorm.Model
 	BoreDiameter float64 `gorm:"index:idx_cartridges__borediameter,not null"`
 	Name         string  `gorm:"index:idx_cartridges__name,unique,not null"`
-	Size         string  `gorm:"index:idx_cartridges__size"`
+	Size         string  `gorm:"index:idx_cartridges__size,not null"`
 }
 
 func (t Cartridge) String() string {
@@ -28,7 +34,8 @@ func NewCartridge(name string, size string, bore float64) Cartridge {
 
 func GetCartridges() ([]Cartridge, error) {
 	var c []Cartridge
-	db, err := gorm.Open(sqlite.Open("mlrb.db3"), &gorm.Config{})
+	db, err := getDb()
+
 	if err != nil {
 		return nil, err
 	}
@@ -41,24 +48,39 @@ func GetCartridges() ([]Cartridge, error) {
 }
 
 func UpsertCartridge(c Cartridge) error {
-	err := Bootstrap()
+	db, err := getDb()
 	if err != nil {
 		return err
 	}
 
-	db, err := gorm.Open(sqlite.Open("mlrb.db3"), &gorm.Config{})
-	if err != nil {
-		return err
-	}
+	dbc := db.Create(&c)
+	if dbc.Error != nil {
+		if errors.Is(dbc.Error, sqlite3.Error) {
 
-	gormdb := db.Create(&c)
-	if gormdb.Error != nil {
-		logrus.WithError(gormdb.Error).Errorf("There was a problem trying to upsert the cartridge %s.", gormdb.Error)
+			// Code = 19 | ExtendedCode = 2067 | err = UNIQUE constraint failed: cartridges.name
+			logrus.WithError(dbc.Error).Warnf("This cartridge name already exists %s", c.Name)
+		} else {
+			logrus.WithError(dbc.Error).Errorf("There was a problem trying to upsert the cartridge %s.", dbc.Error)
+		}
 	}
 
 	return nil
 
 }
+
+func getDb() (*gorm.DB, error) {
+	err := Bootstrap()
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := gorm.Open(sqlite.Open(DatabaseName), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
 func Bootstrap() error {
 	db, err := gorm.Open(sqlite.Open("mlrb.db3"), &gorm.Config{})
 	if err != nil {
