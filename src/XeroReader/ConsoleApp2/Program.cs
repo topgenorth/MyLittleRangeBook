b028f5d83ad88af55b51a93a3e45aff4cf840a5a
Tom Opgenorth <tom@opgenorth.net>
2024-09-22 10:06:03 -0600

    Learning the FitSDK.

--- src/XeroReader/ConsoleApp2/Program.cs
+++ src/XeroReader/ConsoleApp2/Program.cs
@@ -1,48 +1,12 @@
-﻿using ConsoleAppFramework;
-using Dynastream.Fit;
+﻿using ConsoleApp2;
+using ConsoleAppFramework;
 using Serilog;
 
 await using var log = new LoggerConfiguration().WriteTo.Console().WriteTo.Debug().CreateLogger();
-
-/*
-var filename = "C:\\Users\\tom.opgenorth\\Dropbox\\Firearms\\Shot_Sessions\\09-08-2024_18-04-40.fit";
-
-
-log.Information("Processing file {filename}", filename);
-
-var fitListener = new FitListener();
-var decodeDemo = new Decode();
-decodeDemo.MesgEvent += fitListener.OnMesg;
-
-log.Information("Decoding...");
-await using var fitSource = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
-decodeDemo.Read(fitSource);
-
-var fitMessages = fitListener.FitMessages;
-foreach (var msg in fitMessages.DeviceInfoMesgs)
-{
-    log.Information($"Timestamp: {msg?.GetTimestamp().ToString()}, SoftwareVersion: {msg?.GetSoftwareVersion()}");
-}
-
-foreach (var msg in fitListener.FitMessages.FileIdMesgs)
-{
-    log.Information(
-        $"File ID Timestamp: {msg?.GetManufacturer()}, Serial #: {msg?.GetGarminProduct()} {msg?.GetSerialNumber()}");
-    ;
-}
-
-foreach (var msg in fitMessages.ChronoShotSessionMesgs)
-{
-    var t = msg?.GetTimestamp();
-    log.Information($"Session Timestamp: {msg?.GetTimestamp().ToString()}, Shot Count: {msg?.GetShotCount()}");
-}
-
-log.Information("Done");
-await Log.CloseAndFlushAsync();*/
-
+Serilog.Log.Logger = log;
 var app = ConsoleApp.Create();
 
-app.Add("", (string msg) => log.Information(msg));
-app.Add("hello", (string msg) => log.Information($"Hello {msg}"));
-
+// app.Add("", () => log.Information("Seems like you need help"));
+//app.Add("file", (string fileName) => log.Information($"Process the file {fileName}"));
+app.Add<SimpleFitReader>("");
 app.Run(args);
