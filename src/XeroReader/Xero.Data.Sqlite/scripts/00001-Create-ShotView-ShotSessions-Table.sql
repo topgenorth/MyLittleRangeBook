81627da956bdacdc93ae66e25fc0274f723680c6
Tom Opgenorth <tom@opgenorth.net>
2024-12-28 11:37:01 -0700

    Add another index to the tables, fix the datatype of a column.

--- src/XeroReader/Xero.Data.Sqlite/scripts/00001-Create-ShotView-ShotSessions-Table.sql
+++ src/XeroReader/Xero.Data.Sqlite/scripts/00001-Create-ShotView-ShotSessions-Table.sql
@@ -1,15 +1,18 @@
 CREATE TABLE shotview_sessions
 (
-    id TEXT NOT NULL CONSTRAINT shotview_sessions_pk PRIMARY KEY,
+    id                TEXT    NOT NULL
+        CONSTRAINT shotview_sessions_pk PRIMARY KEY,
     session_date      TEXT    NOT NULL,
-    name              integer NOT NULL,
-    projectile_weight integer          DEFAULT 0 NOT NULL,
-    projectile_type   TEXT    NOT NULL,
+    name              TEXT    NOT NULL,
+    projectile_weight integer NOT NULL DEFAULT 0,
+    projectile_type   TEXT    NOT NULL DEFAULT 'Rifle',
     projectile_units  TEXT    NOT NULL DEFAULT 'grains',
     velocity_units    TEXT    NOT NULL DEFAULT 'fps',
-    notes             TExT             DEFAULT NULL,
+    notes             TEXT             DEFAULT NULL,
     modification_date TEXT    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     creation_date     TEXT    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE INDEX shotview_session_session_date_idx ON shotview_sessions (session_date);
+CREATE UNIQUE INDEX shotview_sessions_name_uindex ON shotview_sessions (name);
+CREATE INDEX shotview_session_modification_dated_idx ON shotview_sessions (modification_date);
