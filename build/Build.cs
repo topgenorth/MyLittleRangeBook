3a5713065eb2fdb1e5f55e6f1fa0baa6dca70d00
Tom Opgenorth <tom@opgenorth.net>
2024-12-30 11:12:32 -0700

    nuke Clean

--- build/Build.cs
+++ build/Build.cs
@@ -1,44 +1,82 @@
-using System;
-using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
-using Nuke.Common.Execution;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Git;
 using Nuke.Common.IO;
-using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
+using Serilog;
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-
-    public static int Main () => Execute<Build>(x => x.Compile);
-
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+
+    /// Support plugins are available for:
+    /// - JetBrains ReSharper        https://nuke.build/resharper
+    /// - JetBrains Rider            https://nuke.build/rider
+    /// - Microsoft VisualStudio     https://nuke.build/visualstudio
+    /// - Microsoft VSCode           https://nuke.build/vscode
+
+    [GitRepository] readonly GitRepository Repository;
+    [CI] readonly GitHubActions GitHubActions;
+    const string MasterBranch = "master";
+    const string DevelopBranch = "develop";
+    AbsolutePath OutputDirectory => RootDirectory / "output";
+    AbsolutePath SourceDirectory => RootDirectory / "src/XeroReader";
 
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            Log.Debug("Cleaning the solution: {source}", SourceDirectory);
+            SourceDirectory.GlobDirectories("*/bin", "*/obj").DeleteDirectories();
+            OutputDirectory.CreateOrCleanDirectory();
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            Log.Verbose("Restoring dependencies.");
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            Log.Verbose("Compile the solution.");
         });
 
+    Target UnitTests => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            Log.Verbose("Running all unit tests");
+        });
+
+    Target Install => _ => _
+        .DependsOn(UnitTests)
+        .Requires(() => Repository.IsOnMainOrMasterBranch())
+        .Executes(() =>
+        {
+            var installDir = "~/.local/bin/";
+            Log.Information("Installing the application to {installDir} ", installDir);
+        });
+
+    Target Print => _ => _
+        .Executes(() =>
+        {
+            Log.Information("Commit = {Value}", Repository.Commit);
+            Log.Information("Branch = {Value}", Repository.Branch);
+            Log.Information("Tags = {Value}", Repository.Tags);
+
+            Log.Information("main branch = {Value}", Repository.IsOnMainBranch());
+            Log.Information("main/master branch = {Value}", Repository.IsOnMainOrMasterBranch());
+            Log.Information("release/* branch = {Value}", Repository.IsOnReleaseBranch());
+            Log.Information("hotfix/* branch = {Value}", Repository.IsOnHotfixBranch());
+
+            Log.Information("Https URL = {Value}", Repository.HttpsUrl);
+            Log.Information("SSH URL = {Value}", Repository.SshUrl);
+        });
+
+    public static int Main() => Execute<Build>(x => x.Compile);
 }
