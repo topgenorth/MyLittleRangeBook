5624cff041a9f5098a8ec9d75bdf222afc8324df
Tom Opgenorth <tom@opgenorth.net>
2025-09-23 19:39:12 -0600

    Playing with F#

---
+++ src/MyLittleRangeBook.Model/Types.fs
@@ -0,0 +1,45 @@
+﻿namespace MyLittleRangeBook.Model
+
+open System
+
+module RangeLogTypes = 
+    type MeasuredValue =
+        | Velocity of value: float * units: string
+        | Weight of value: int * units: string
+        | CBTO of length: float * units: string
+        | COAL of length: float * units: string
+
+        member t.DisplayFieldName =
+            match t with
+            | Velocity _ -> "Velocity"
+            | Weight _ -> "Weight"
+            | CBTO _ -> "CBTO"
+            | COAL _ -> "COAL"
+
+    type RangeTrip =
+        { Id: string
+          TripDate: DateTime
+          RoundsFired: int
+          Ammo: string
+          RangeName: string
+          Notes: string
+          Firearm: string }
+
+    type PowderCharge =
+        { Id: string
+          PowderName: string
+          Weight: float
+          Units: string }
+
+    type LoadData =
+        { Id: string
+          Charge: PowderCharge
+          Length: MeasuredValue
+          Brass: string }
+
+    [<Struct>]
+    type ShotData =
+        { Id: string
+          MuzzleVelocity: float
+          Unit: string
+          Omit: bool }
---
+++ src/XeroReader/ShotViewCsv.App/Program.fs
@@ -0,0 +1,16 @@
+﻿open Deedle
+
+let files =
+    [| "/home/tom/Dropbox/Firearms/MyLittleRangeBook/ShotView/6x45/6x45_750_gr_Hornad_2025-05-20_11-58-18.csv"
+       "/home/tom/Dropbox/Firearms/MyLittleRangeBook/ShotView/6x45/6x45_750_gr_Hornad_2025-05-20_11-33-59.csv"
+       "/home/tom/Dropbox/Firearms/MyLittleRangeBook/ShotView/6x45/6x45_750_gr_Hornad_2025-05-11_16-47-35.csv" |]
+
+let f1 = Frame.ReadCsv(files[0])
+let f2 = Frame.ReadCsv(files[1])
+let f3 = Frame.ReadCsv(files[2])
+
+let f4 = f1.Join f2
+f4.SaveCsv("/home/tom/Dropbox/Firearms/MyLittleRangeBook/ShotView/6x45/f4.csv")
+
+let f5= f4.Join f4
+f5.SaveCsv("/home/tom/Dropbox/Firearms/MyLittleRangeBook/ShotView/6x45/f5.csv")
