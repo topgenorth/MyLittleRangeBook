9b5611e215d14fdda4ee4a6fa9c6f5039a0bbb6b
Tom Opgenorth <tom@opgenorth.net>
2026-05-01 22:14:04 -0600

    Missed some file.s.

---
+++ src/mlrb/MyLittleRangeBook/Config/BootstrapFuncs.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Nodes;
+using FluentResults;
+using Serilog;
+
+namespace MyLittleRangeBook.Config
+{
+    public class BootstrapFuncs
+    {
+        /// <summary>
+        ///     Check to see if there is a Logging section in the appsettings.json file. If not, create one.
+        /// </summary>
+        public static readonly Func<JsonNode?, Result> LoggingSectionBootstrapper = rootNode =>
+        {
+            ArgumentNullException.ThrowIfNull(rootNode);
+
+            if (rootNode["Logging"] is not null)
+            {
+                Log.Debug("Logging configuration already exists in appsettings.json. Skipping Logging bootstrapper.");
+
+                return Result.Ok();
+            }
+
+            string json = ConfigurationExtensions.DefaultLoggingSectionJson().Result;
+            rootNode["Logging"] ??= JsonNode.Parse(json);
+
+            return Result.Ok();
+        };
+    }
+}
