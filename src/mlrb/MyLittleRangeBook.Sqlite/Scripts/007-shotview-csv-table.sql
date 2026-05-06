e4056894c522032006bf49c35822b3f01a4e688c
Tom Opgenorth <tom@opgenorth.net>
2026-05-06 16:40:32 -0600

    Create migration for the ShotViewFiles table

---
+++ src/mlrb/MyLittleRangeBook.Sqlite/Scripts/007-shotview-csv-table.sql
@@ -0,0 +1,35 @@
+﻿CREATE TABLE IF NOT EXISTS ShotViewFiles
+(
+    RowId    INTEGER PRIMARY KEY AUTOINCREMENT,
+    Id       TEXT                           not null, --NanoID unique key.
+    FileName TEXT                           not null,
+    MimeType TEXT default 'text/csv'        not null,
+    Contents TEST                           not null, -- The file contents as TEXT.
+    Created  TEXT default CURRENT_TIMESTAMP not null, -- The date the record was created.
+    Modified TEXT default CURRENT_TIMESTAMP not null, -- The date the file was last modified.
+    CONSTRAINT UQ_FileFiles UNIQUE (ID)
+);
+CREATE UNIQUE INDEX IF NOT EXISTS IX_ShotViewFiles_Id
+    ON FitFiles (Id);
+CREATE UNIQUE INDEX IF NOT EXISTS IX_ShotViewFiles_FileName
+    ON FitFiles (FileName);
+
+CREATE TABLE IF NOT EXISTS SimpleRangeEvent_ShotViewFiles
+(
+    RowId              INTEGER PRIMARY KEY AUTOINCREMENT,
+    SimpleRangeEventId TEXT NOT NULL, -- NanoId of the SimpleRangeEvent record.
+    ShotViewFileId     TEXT NOT NULL, -- NanoId of the ShotView file record.
+
+    CONSTRAINT FK_SimpleRangeEvent_ShotViewFiles_SimpleRangeEvents
+        FOREIGN KEY (SimpleRangeEventId)
+            REFERENCES SimpleRangeEvents (Id)
+            ON DELETE CASCADE,
+
+    CONSTRAINT FK_SimpleRangeEvent_ShotViewFiles_ShotViewFiles
+        FOREIGN KEY (ShotViewFileId)
+            REFERENCES ShotViewFiles (Id)
+            ON DELETE CASCADE,
+
+    CONSTRAINT UQ_SimpleRangeEvent_FitFiles_Event_FitFile
+        UNIQUE (SimpleRangeEventId, ShotViewFileId)
+);
