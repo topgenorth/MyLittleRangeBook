e026dda7069c90bb5857e9da1e1249229fe6d647
Tom Opgenorth <tom@opgenorth.net>
2026-06-02 19:51:46 -0600

    Update cleanup script to exclude non-firearm streams

--- src/mlrb/sql-scripts/cleanup-asset-imports.sql
+++ src/mlrb/sql-scripts/cleanup-asset-imports.sql
@@ -1,3 +1,6 @@
-﻿DELETE FROM main.events WHERE stream_type='mlrb-asset';
-DELETE FROM main.event_streams WHERE stream_type='mlrb-asset';
-DELETE FROM main.asset_files WHERE mime_type='text/csv';
+﻿DELETE FROM main.events
+       WHERE stream_type<>'firearm';
+DELETE FROM main.event_streams
+       WHERE stream_type<>'firearm';
+DELETE FROM main.asset_files
+       WHERE mime_type='text/csv';
