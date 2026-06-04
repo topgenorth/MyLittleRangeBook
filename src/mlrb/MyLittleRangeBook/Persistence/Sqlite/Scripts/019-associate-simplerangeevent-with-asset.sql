6e4d72c21c07b3b1f4fcb0a7ef650d56f73d91ff
Tom Opgenorth <tom@opgenorth.net>
2026-06-04 07:52:20 -0600

    Create the migration to associate simple range event with an asset.

---
+++ src/mlrb/MyLittleRangeBook/Persistence/Sqlite/Scripts/019-associate-simplerangeevent-with-asset.sql
@@ -0,0 +1,17 @@
+﻿DROP TABLE IF EXISTS main.asset_files_simplerangeevents;
+CREATE TABLE asset_files_simplerangeevents
+(
+    row_id                INTEGER PRIMARY KEY AUTOINCREMENT,
+    simple_range_event_id TEXT NOT NULL CONSTRAINT FK_assetfilessimplerangeevents_simplerangeevents
+        REFERENCES SimpleRangeEvents (id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    asset_id         TEXT NOT NULL
+        CONSTRAINT FK_assetfilessimplerangeevents_assetfiles
+            REFERENCES asset_files (id)
+            ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT UQ_assetfilessimplerangeevents_firearmid_assetfileid
+        UNIQUE (simple_range_event_id, asset_id)
+);
+
+CREATE UNIQUE INDEX IX_assetfilessimplerangeevents_firearmid_assetid
+    ON asset_files_simplerangeevents (simple_range_event_id, asset_id);
