b8317bf24d1ca1fbf6f259035678b2a5d0c8a467
Tom Opgenorth <tom@opgenorth.net>
2026-06-14 19:39:31 -0600

    Script to nuke any firearm aggregrates or firearms.

---
+++ src/mlrb/sql-scripts/reset-firearms-table.sql
@@ -0,0 +1,8 @@
+﻿-- Script to delete all the rows from the firearms table, and then clean out the events and events streams.
+
+-- noinspection SqlWithoutWhere
+DELETE FROM firearms;
+
+DELETE FROM events WHERE stream_type = 'firearm' AND id NOT IN (SELECT id FROM firearms);
+DELETE FROM event_streams WHERE stream_type = 'firearm' AND id NOT IN (SELECT id FROM firearms);
+DELETE FROM event_streams WHERE stream_type = 'firearm' AND id NOT IN (SELECT id FROM events);
