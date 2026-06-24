ba1600dcdd9ced52c9d8888f827562afb2686f75
Tom Opgenorth <tom@opgenorth.net>
2026-06-24 10:25:23 -0600

    Default implementation for EventType on an IDomainEvent.

--- src/mlrb/MyLittleRangeBook/EventSourcing/IDomainEvent.cs
+++ src/mlrb/MyLittleRangeBook/EventSourcing/IDomainEvent.cs
@@ -1,4 +1,5 @@
-﻿using MyLittleRangeBook.Models;
+﻿using System.Reflection;
+using MyLittleRangeBook.Models;
 
 namespace MyLittleRangeBook.EventSourcing
 {
@@ -6,5 +7,28 @@ namespace MyLittleRangeBook.EventSourcing
     {
         MlrbId         StreamId    { get; }
         DateTimeOffset OccurredUtc { get; }
+
+        /// <summary>
+        /// Return the value of the EventTypeAttribute.Name property if the Event has one.
+        /// </summary>
+        string EventType
+        {
+            get
+            {
+                Type domainEventType = GetType();
+
+                EventTypeAttribute? eventTypeAttribute =
+                    domainEventType.GetCustomAttribute<EventTypeAttribute>(false);
+
+                if (eventTypeAttribute is not null)
+                {
+                    return eventTypeAttribute.Name;
+                }
+
+                string typeName = domainEventType.Name;
+                return string.Concat(typeName.Select((c, i) =>
+                    i > 0 && char.IsUpper(c) ? "-" + char.ToLower(c) : char.ToLower(c).ToString()));
+            }
+        }
     }
 }
