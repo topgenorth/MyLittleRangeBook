9feb437681db677ac5ccf1b3798f626dad87d480
Tom Opgenorth <tom@opgenorth.net>
2026-07-18 09:46:10 -0600

    Delete files.

--- src/mlrb/sql-scripts/count-rounds-per-firearm.sql
+++
@@ -1,6 +0,0 @@
-﻿SELECT
-    simple_range_events.firearm_name AS FirearmName,
-    COALESCE(SUM(simple_range_events.rounds_fired), 0) AS TotalRoundsFired
-FROM simple_range_events
-GROUP BY simple_range_events.firearm_name
-ORDER BY simple_range_events.firearm_name;
---
+++ src/mlrb/sql-scripts/nuke-data.sql
@@ -0,0 +1,23 @@
+﻿-- Delete data from all tables except cartridges
+DELETE
+FROM asset_files;
+DELETE
+FROM asset_files_firearms;
+DELETE
+FROM asset_files_simple_range_events;
+DELETE
+FROM event_streams;
+DELETE
+FROM events;
+DELETE
+FROM firearms;
+DELETE
+FROM firearms_simple_range_events;
+DELETE
+FROM simple_range_events;
+
+-- Vacuum the database to reclaim space
+VACUUM;
+
+-- Update index statistics
+ANALYZE;
