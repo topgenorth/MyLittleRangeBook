c0a10d25ef82aa88f71b6227fb4dc10cf3069e8e
Tom Opgenorth <tom@opgenorth.net>
2026-09-17 09:24:24 -0600

    Refactor Garmin ShotView CSV file command: enhance method to include firearm parameter, improve error handling, and add event logging

--- src/mlrb/MyLittleRangeBook.CLI/ParseGarminShotViewCsvFileCommand.cs
+++ src/mlrb/MyLittleRangeBook.CLI/ParseGarminShotViewCsvFileCommand.cs
@@ -1,29 +1,53 @@
 ﻿using ConsoleAppFramework;
+using Fisher;
 using MyLittleRangeBook.Console;
+using MyLittleRangeBook.Firearms;
 
 namespace MyLittleRangeBook
 {
+    public record struct GarminShotViewFileLoaded(string FirearmName, string FileName, string Contents, DateTimeOffset OccurredUtc);
     [RegisterCommands("garmin")]
     public class ParseGarminShotViewCsvFileCommand
     {
-        readonly ICliDisplay _cliDisplay;
-        public ParseGarminShotViewCsvFileCommand(ICliDisplay cliDisplay)
+        readonly ICliDisplay      _cliDisplay;
+        readonly IDocumentSession _session;
+        readonly IFirearmsService _firearmsService;
+
+        public ParseGarminShotViewCsvFileCommand(ICliDisplay cliDisplay, IDocumentSession session, IFirearmsService firearmsService)
         {
-            _cliDisplay = cliDisplay;
+            _cliDisplay           = cliDisplay;
+            _session              = session;
+            _firearmsService = firearmsService;
         }
 
+        /// <summary>
+        /// Adds a Garmin ShotView CSV file. Capture the CSV file, and then covert it to a document.
+        /// </summary>
+        /// <param name="firearm"></param>
+        /// <param name="file"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         [Command("add-shotview")]
-        public async Task<int> AddShotViewFile(string file, CancellationToken cancellationToken = default)
+        public async Task<int> AddShotViewFile(string firearm, string file, CancellationToken cancellationToken = default)
         {
             _cliDisplay.PrintCommandHeader("Import Garmin ShotView CSV file.");
+            int    returnCode   = -1;
             if (!File.Exists(file))
             {
                 _cliDisplay.PrintFailure($"File {file} not found.");
-                return ReturnCodes.SHOTVIEW_FILE_NOT_FOUND;
+                returnCode= ReturnCodes.SHOTVIEW_FILE_NOT_FOUND;
+                goto Exit;
+            }
+
+            var rFirearmId = await _firearmsService.FetchStreamIdForFirearm(firearm, cancellationToken);
+            if (rFirearmId.IsFailed)
+            {
+                _cliDisplay.PrintFailure($"Firearm {firearm} not found.");
+                returnCode = ReturnCodes.SHOTVIEW_FILE_READ_FAILURE;
+                goto Exit;
             }
 
             string fileContents = string.Empty;
-
             try
             {
                 fileContents = await File.ReadAllTextAsync(file, cancellationToken);
@@ -31,11 +55,20 @@ namespace MyLittleRangeBook
             catch (Exception ex)
             {
                 _cliDisplay.PrintFailure($"Error reading file {file}: {ex.Message}");
-                return ReturnCodes.SHOTVIEW_FILE_READ_FAILURE;
+                returnCode =  ReturnCodes.SHOTVIEW_FILE_READ_FAILURE;
+                goto Exit;
             }
 
-            int    returnCode   = -1;
+            List<object> events = [];
+            var e1 = new GarminShotViewFileLoaded(firearm, file, fileContents, DateTimeOffset.UtcNow);
+            events.Add(e1);
 
+            _session.Events.Append(rFirearmId.Value, events);
+            await _session.SaveChangesAsync(cancellationToken);
+
+            returnCode = ReturnCodes.SUCCESS;
+
+Exit:
             if (returnCode == ReturnCodes.SUCCESS)
             {
                 _cliDisplay.PrintSuccess("Parsed the file.");
